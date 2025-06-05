4c91b34c016d18d16cc5e23cdb2bee8946f2059f
Dvinaja-hub <damian.yoda2011@gmail.com>
2025-06-05 17:31:58 -0500

    Update parameters.py

--- Lesson3/parameters.py
+++ Lesson3/parameters.py
@@ -1,8 +1,8 @@
 #You want the function below to print a greeting message to the screen.
 # This code will not be executed until it is 'called'.
-def say_hello(Bob):
-    print(f'Hello {Austin}')
+def say_hello('name','city'):
+    
 
-Hello welcome to are (Austin) (Bob)
+Hello welcome to are (city) (name)
 # Our function is defined above, so now we can 'call' it
-say_hello('Bob')
+say_hello('Bob','Austin")
